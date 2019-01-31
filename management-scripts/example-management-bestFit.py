cc701beee3944c634b8d81c55e1db1fdf77f4fc4
Alessandro Pagiaro <alessandropagiaro@gmail.com>
2019-01-31 19:20:02 +0100

    Update example-management-bestFit.py

--- management-scripts/example-management-bestFit.py
+++ management-scripts/example-management-bestFit.py
@@ -14,7 +14,7 @@ def bestFit(cpu, mem):
     if tmp%30 == 0:
         print("***********")
         for dev in devices:
-            print(dev["ipAddress"])
+            print(dev["ipAddress"], (dev["capabilities"]["nodes"][0]["cpu"]["available"], dev["capabilities"]["nodes"][0]["memory"]["available"]))
     tmp += 1
     best_fit = devices[0]
     return best_fit["ipAddress"]
@@ -76,7 +76,6 @@ for DEPLOYMENT_NUMBER in range(130, 200, 10):
         _, myapp1 = fg.create_myapp(localapp["localAppId"], dep)
 
         deviceIp = bestFit(100, 32)
-
         code, res = fg.install_app(dep, [deviceIp], resources={"resources":{"profile":"c1.tiny","cpu":100,"memory":32,"network":[{"interface-name":"eth0","network-name":"iox-bridge0"}]}})
         trial = 0
         while code == 400:
