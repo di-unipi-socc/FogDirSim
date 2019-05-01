9f388bce7ca5266f220864535eeed2081dd8c1bc
Samuele Maci <macisamuele@gmail.com>
2019-05-01 16:13:07 +0200

    Implement POST /v1/appmgr/tokenservice and DELETE /v1/appmgr/tokenservice/{token}

--- fog_director_simulator/pyramid/fake_fog_director/views/v1_appmgr_apps_local_application_id_version.py
+++ fog_director_simulator/pyramid/fake_fog_director/views/v1_appmgr_apps_local_application_id_version.py
@@ -1,3 +1,7 @@
+from typing import Any
+from typing import Dict
+
+from pyramid.httpexceptions import HTTPOk
 from pyramid.request import Request
 from pyramid.view import view_config
 
@@ -7,8 +11,10 @@ from pyramid.view import view_config
     renderer='json',  # Needed due to https://github.com/striglia/pyramid_swagger/issues/236
     request_method='DELETE',
 )
-def delete_v1_appmgr_apps_local_application_id_version(request: Request) -> None:
+def delete_v1_appmgr_apps_local_application_id_version(request: Request) -> Dict[str, Any]:
     request.database_logic.delete_application(
         localAppId=request.swagger_data['local_application_id'],
         version=request.swagger_data['version'],
     )
+
+    raise HTTPOk()
--- tests/pyramid/fake_fog_director/v1_appmgr_tokenservice_test.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from webtest import TestApp
-
-
-@pytest.mark.xfail
-def test_get_v1_appmgr_tokenservice_without_tokens(testapp):
-    # type: (TestApp) -> None
-    response = testapp.get('/api/v1/appmgr/tokenservice', expect_errors=True)
-    assert response.status_code == 400
