b111922f483471846f8e43f3b989d3622194a873
Alessandro Pagiaro <alessandropagiaro@gmail.com>
2019-06-10 11:23:29 +0200

    Fix random generation

--- fog_director_simulator/metrics_collector/__init__.py
+++ fog_director_simulator/metrics_collector/__init__.py
@@ -1,21 +1,33 @@
+from functools import lru_cache
 from functools import wraps
+from random import gauss as normal
 from typing import Any
 from typing import Callable
 from typing import TypeVar
 
+import scipy.stats as stats
 from numpy.random import choice
-from numpy.random import normal
+from scipy.stats._continuous_distns import truncexpon_gen
 from sqlalchemy.exc import SQLAlchemyError
-
+# from numpy.random import normal
 
 Func = TypeVar('Func', bound=Callable[..., Any])
 
 
+@lru_cache(maxsize=256)
+def get_scipy_truncnorm(mean: float, std_deviation: float, lower_bound: float, upper_bound: float) -> truncexpon_gen:
+    return stats.truncnorm(
+        (lower_bound - mean) / std_deviation, (upper_bound - mean) / std_deviation, loc=mean, scale=std_deviation)
+
+
 def random_sample(mean: float, std_deviation: float, lower_bound: float, upper_bound: float) -> float:
+    if lower_bound == upper_bound:
+        return lower_bound
     sample = normal(mean, std_deviation)
     while not (lower_bound < sample < upper_bound):
         sample = normal(mean, std_deviation)
     return sample
+    # return get_scipy_truncnorm(mean, std_deviation, lower_bound, upper_bound).rvs()
 
 
 def random_flag(probability_of_true: float) -> bool:
